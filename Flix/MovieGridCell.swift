6b3a06be1713f8b2dae4bbebde8ef305131f74d5
Joshua Tavassolikhah <joshtavassolikhah@yahoo.com>
2019-01-26 01:55:54 -0800

    Added collection view and movie details view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Josh Tav on 1/26/19.
+//  Copyright © 2019 JoshTav. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
